e93953ec118f1d19b551161534f4783824c55e8b
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-05-03 14:42:39 +0200

    feat: add toggle button

--- background.js
+++ background.js
@@ -1,22 +1,37 @@
 
+browser.browserAction.setBadgeBackgroundColor({color: 'white'});
+browser.browserAction.setBadgeText({text: "off" });
+
+let online=false;
+
 async function handleCreated(info) {
+    if(online){
+        console.log('handleCreated:', info.id, info.url);
 
-    console.log('handleCreated:', info.id, info.url);
+        try {
+            browser.downloads.cancel(info.id);
+        }catch(e) {
+            console.log(`Error: ${e}`);
+        }
 
-    try {
-        browser.downloads.cancel(info.id);
-    }catch(e) {
-        console.log(`Error: ${e}`);
+        try {
+            const resp =  await browser.runtime.sendNativeMessage("fwdl", info.url);
+            console.log("Received " + resp);
+        }catch(e) {
+            console.log(`Error: ${e}`);
+        }
+
     }
+}
 
-    try {
-        const resp =  await browser.runtime.sendNativeMessage("fwdl", info.url);
-        console.log("Received " + resp);
-    }catch(e) {
-        console.log(`Error: ${e}`);
-    }
+async function handleClick(tab) {
 
+  // toggle state
+  online = !online
+  browser.browserAction.setBadgeText({tabId: tab.id, text: (online?"on":"off") });
 }
 
 browser.downloads.onCreated.addListener(handleCreated);
+browser.browserAction.onClicked.addListener(handleClick);
+
 
